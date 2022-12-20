84eba170b24a1decb0b52a1522c41f21a02e4711
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-20 21:29:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const PORT = 5555;
+const { PORT = 3030 } = process.env;
 const app = express();
 
 app.use(cors());
